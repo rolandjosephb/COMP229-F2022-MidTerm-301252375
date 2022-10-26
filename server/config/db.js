5d400b69a503ec20687aa7262381d0b8bfe5df9f
Roland <rolandjosephb@yahoo.com>
2022-10-26 10:09:36 -0400

    Midterm

---
+++ server/config/db.js
@@ -0,0 +1,7 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb+srv://rolandjosephb:206v15ALdw@rolandmongodbserver.eejihcn.mongodb.net/book_list?retryWrites=true&w=majority",
+ 
+
+  "Secret": 'SomeSecret'
+};
